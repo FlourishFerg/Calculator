b074683dbb95e533185d3fb0188f877cdfa95d79
FlourishFerg <flourishferguson@gmail.com>
2025-10-21 12:04:48 +0100

    Add sum logic, controller and unit test

---
+++ src/main/java/com/example/calculator/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.example.calculator;
+
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+}
